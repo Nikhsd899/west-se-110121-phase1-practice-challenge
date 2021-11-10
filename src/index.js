104550ea6e3559be507cbecf363f57e5e85cee18
Nicholas <nikhsd899@outlook.com>
2021-11-10 11:16:18 -0800

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-// Your code here
+function () {
+    fetch()
+    .then(resp => resp.json())
+    .then(forEach(renderName))
+}
+const BASE_URL = 'http://localhost:3000/characters';
+
+function allCharactersData() {
+    
+}
+[
+    {
+        "id": 1,
+        "name": "Mr. Cute",
+        "image": "https//thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif"
+        "votes": 0
+    },
+    {
+        "id": 2,
+        "name": "Mr. Monkey,
+        "image": "https//thumbs.gfycat.com/FatalInnocentAmericanshorthair-max-1mb.gif",
+        "votes": 0
+    },
+]
+
